a8aa1add1aafad271191ffd3bf8149d8ce739cb4
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-11-27 12:26:55 -0600

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uah.cs321.termproject;
+
+/**
+ *
+ * @author adrianalanier
+ */
+public class Quiz {
+    private int numQuestions;
+    
+    public void Quiz()
+    {
+    
+    }
+    
+    public void addQuestion()
+    {
+    
+    }
+    
+    
+    
+    
+}
