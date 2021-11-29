b3a166c2733471a2b3f6cd40701271e503dea2d7
Brad <bms91195@gmail.com>
2021-11-28 19:12:32 -0600

    Added Grade and GradeBook classes

---
+++ Grade.java
@@ -0,0 +1,36 @@
+package Grader;
+import java.util.*;
+
+/** Object that holds the score details for a specific lesson.**/
+public class Grade {
+    private String LessonID;    // Correlates to the LessonID of a lesson.
+    private int QuizScore;   // The final score of a quiz.
+    
+    /** Grade Class Constructor **/
+    public void Grade() {
+    }
+    
+    /** Sets the Grade for a specific grade
+    * @param ID: Lesson ID is a unique string identifier for each Lesson
+    * @param ScoreGrade: Grade value for specific lesson**/
+    public void setGrade(String ID, int ScoreGrade) {
+        LessonID = ID;
+        QuizScore = ScoreGrade;
+    }
+    
+    /** Returns the QuizScore of the Grade object
+     * @return double QuizScore
+     */
+    public int getScore() {
+        return QuizScore;
+    }
+    
+    /** Updates the score for a lesson that already has a QuizScore **/
+    public void updateScore(int Score) {
+        QuizScore = Score;
+    }
+    
+    String GetLessonID() {
+        return LessonID;
+    }
+}
---
+++ GradeBook.java
@@ -0,0 +1,42 @@
+package Grader;
+import java.util.*;
+
+public class GradeBook {
+
+    private Hashtable<String, Grade> GradeBookScores = new Hashtable<String, Grade>();
+    //private Dictionary<String, Grade> GradeBookScores;
+    
+    /** Default GradeBook constructor **/
+    public GradeBook() {
+    }
+    
+    /** Add new grade object to the GradeBook.
+     * @param LessonID: Lesson ID is a unique identifier for each Lesson
+     * @param grade: Grade Object stores details about a users grade for a specific lesson. **/
+    public void addScore(String LessonID, Grade grade) {
+        GradeBookScores.put(LessonID, grade);
+    }
+    
+    /** Return the quiz score related to the LessonID **/
+    public int getScore(String LessonID) {
+        Grade grade = GradeBookScores.get(LessonID);
+        return grade.getScore();
+    }
+    
+    /** Return the int value of the size of the GradeBook. **/
+    public int getGradeBookSize() {
+        return GradeBookScores.size();
+    }
+    
+    /** This method will return an Enumeration of keys (LessonID's) **/
+    public Enumeration<String> getGradeBookIDs() {
+        return GradeBookScores.keys();
+        
+        /* Example: Use the following code to print each LessonID
+        Enumeration keys = Gradebook1.getGradeBookIDs();
+        while (keys.hasMoreElements()) {
+            System.out.println(keys.nextElement());
+        }
+        */
+    }
+}
