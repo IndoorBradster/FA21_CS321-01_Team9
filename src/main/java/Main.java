a38ba4e70d6587c534aebf7610d9b248bc53c3a2
Brad <bms91195@gmail.com>
2021-12-03 11:42:54 -0600

    Rudimentary UI
    Added a UI file and adjusted file structure. The UI has no integration, it exists now only as a functional design.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,6 +38,30 @@ public class Main
 {
     public static void main(String args[])
     {
+		// <-- ui implementation ---------------------------------------------------- //
+		/* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(EducatorUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new EducatorUI().setVisible(true);
+        });
+		// -- ui implementation ----------------------------------------------------> //
+		
         String Username, Password;
         accountcontrol obj = new accountcontrol();
         quiz obj2 = new quiz();
