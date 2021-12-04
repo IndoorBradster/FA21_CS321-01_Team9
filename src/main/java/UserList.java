d3cc82a64716f700ad777719752dca81ad19428d
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-12-04 14:27:31 -0600

    updated some javadoc

--- src/main/java/UserList.java
+++ src/main/java/UserList.java
@@ -1,14 +1,38 @@
 import java.util.HashMap;
 
+
+
+/**
+*Stores all users of the program in a list 
+*
+*/
 public class UserList {
     private final HashMap<String, accountcontrol> UserList = new HashMap<>();
     
+
+/**
+*Constructor
+*
+*/
     public UserList() {
     }
-    
+
+/**
+*Adds a users account names and accountcontrol (user/pass) to list
+*@param accountname name of account
+* @param account accountcontrol object
+*/    
     public void addUser(String accountname, accountcontrol account) {
         UserList.put(accountname, account);
     }
+
+/**
+*Checks to see if passwords match
+*@param username 
+* @param password 
+* @return account
+* @return null 
+*/ 
     
     public accountcontrol getUser(String username, String password) {
         accountcontrol account = UserList.get(username);
