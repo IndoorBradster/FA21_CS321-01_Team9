6fe108395c7f1729d34b58e7a8e28e214296d806
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-12-04 14:08:20 -0600

    added javadoc

--- src/main/java/Lesson.java
+++ src/main/java/Lesson.java
@@ -17,28 +17,52 @@ public class Lesson {
     public void Lesson() {
     }
     
-    // Setters
+// Setters
+/** Set the ID of a lesson
+ * @param ID string to assign to a particular lesson as the ID
+ *  
+*/
     public void setLessonID(String ID) {
         LessonID = ID;
     }
-    
+
+/** Set the title of a lesson
+ * @param Title string to assign to a particular lesson as the title
+ *  
+*/
     public void setLessonTitle(String Title) {
         LessonTitle = Title;
     }
-    
+   
+/** Set the content of a lesson
+ * @param content string to assign to a particular lessons content
+ *  
+*/ 
     public void setLessonContent(String Content) {
         LessonContent = Content;
     }
     
     // Getters
+
+/** get the ID of a lesson
+ * @return LessonID string
+ *  
+*/
     public String getLessonID() {
         return LessonID;
     }
-
+/** get the title of a lesson
+ * @return LessonTitle string
+ *  
+*/
     public String getLessonTitle() {
         return LessonTitle;
     }
-    
+
+/** get the content of a lesson
+ * @return LessonContent string
+ *  
+*/ 
     public String getLessonContent() {
         return LessonContent;
     }
