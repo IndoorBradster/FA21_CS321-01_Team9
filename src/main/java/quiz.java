ab6e707205c3d29587982add60f57f426f9dbcb7
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-12-05 11:01:49 -0600

    added javadoc

--- src/main/java/quiz.java
+++ src/main/java/quiz.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 //package uah.cs321.termproject;
 
-/**
+/*
  *
  * @author adrianalanier
  */
@@ -32,26 +32,41 @@ import java.util.List;
 */
 
 
-
+/**
+ *Represents one quiz
+ * 
+ */
 public class quiz{
 	//function to print introduction
-    private List<Question> questions = new LinkedList<>();
-    private String LessonID;
+    private List<Question> questions = new LinkedList<>();			//list to hold all questions in the quiz
+    private String LessonID;							//matches the question to its lesson
     
     // setters
+/** Assigns the ID of the matching lesson to the quiz
+ * @param lessonID
+ */
     public void setQuizID(String lessonid) {
         LessonID = lessonid;
     }
-    
+	
+/** Adds 1 question to the Question linked list
+ * @param question
+ */
     public void addQuestion(Question question){
         questions.add(question);
     }
     
-    // getters
+// getters
+	
+/** Gets the ID of the matching the lesson, which is now associated with QuizID
+ * @return LessonID
+ */
     public String getQuizID() {
         return LessonID;
     }
-    
+/** Gets the list of questions in the quiz
+ * @return questions
+ */
     public List<Question> getQuestions(){
         return questions;
     }
