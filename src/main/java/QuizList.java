a3cd8fd6e511f36bbc93062fdc2cc99ff476b67f
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-12-05 16:14:57 -0600

    added javadoc

--- src/main/java/QuizList.java
+++ src/main/java/QuizList.java
@@ -7,28 +7,38 @@ import java.util.HashMap;
  * and open the template in the editor.
  */
 
-/**
+/**Represents the collection of all quizzes
  *
- * @author Brad
  */
 public class QuizList {
     // Mapped by LessonID and respective Quiz
     private final HashMap<String, quiz> quizzes = new HashMap<>();
 
-    // Default Constructor
+/** Constructor
+ */
     public QuizList() {
     }
     
-    // Put a quiz in the list
+ /**Adds a quiz to the list
+ *@param LessonID
+ *@param newquiz
+ */
     public void addQuiz(String LessonID, quiz newquiz) {
         quizzes.put(LessonID, newquiz);
     }
     
-    // Return a quiz from the list
+
+ /**Return a quiz from the list
+ *@param LessonID
+ *@return quizzes.get(LessonID)
+ */
     public quiz getQuiz(String LessonID) {
         return quizzes.get(LessonID);
     }
-    
+ /**Assigns score to a quiz
+ *@param LessonID
+ *@param question
+ */
     public void setQuizQuestionScore(String LessonID, Question question) {
         
     }
