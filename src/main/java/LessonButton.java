8d828ce0993fba682a7af469b46897c652d3926f
adrianalanier <90797737+adrianalanier@users.noreply.github.com>
2021-12-05 11:24:39 -0600

    update javadoc

--- src/main/java/LessonButton.java
+++ src/main/java/LessonButton.java
@@ -7,17 +7,21 @@ import javax.swing.JButton;
  * and open the template in the editor.
  */
 
-/**
+/**Lesson button
  *
  * @author Brad
  */
 public class LessonButton<Lesson> extends JButton {
     private Lesson lesson;
-    
+ /**Gets a particular button's lesson
+ *@return lesson 
+ */    
     public Lesson getLesson() {
         return this.lesson;
     }
-    
+ /**Assigns a particular button to a lesson
+ *@param lesson 
+ */
     public void setLesson(Lesson lesson) {
         this.lesson = lesson;
     }
